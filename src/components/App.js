ffa5da1f91396b9911021d3eb6cfc5614f4137bd
gebhartn <nicholas.gebhart@gmail.com>
2020-03-03 14:36:45 -0800

    chore: begin game view
    * renders chart from victoryjs
    * overlaps plot chart w/ lines

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-const App = () => <div>Hello World</div>;
+import PrivateRoute from './PrivateRoute';
+import Login from './Login';
+import Game from './Game';
+
+const App = () => (
+  <>
+    <Route path="/login" component={Login} />
+    <PrivateRoute path="/game" component={Game} />
+  </>
+);
 
 export default App;
