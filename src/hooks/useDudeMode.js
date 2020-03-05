4909ddc81fd8dd5b6e3cfb8bb996a07c3d7dac79
gebhartn <nicholas.gebhart@gmail.com>
2020-03-05 10:36:55 -0800

    chore: adds useDudeMode

---
+++ src/hooks/useDudeMode.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+export const useLocalStorage = (key, init) => {
+  if (typeof key !== 'string') {
+    throw new Error('must be a string');
+  }
+
+  const [storedValue, setStoredValue] = useState(() => {
+    return localStorage.getItem(key)
+      ? JSON.parse(localStorage.getItem(key))
+      : init;
+  });
+
+  const setValue = value => {
+    localStorage.setItem(key, JSON.stringify(value));
+    setStoredValue(value);
+  };
+  return [storedValue, setValue];
+};
+
+export const useDudeMode = () => {
+  const [dudeMode, setDudeMode] = useLocalStorage('dude');
+  useEffect(() => {
+    if (dudeMode) {
+      document.querySelector('body').classList.add('dude-mode');
+    } else {
+      document.querySelector('body').classList.remove('dude-mode');
+    }
+  }, [dudeMode]);
+
+  const toggleDude = e => {
+    e.preventDefault();
+    setDudeMode(!dudeMode);
+  };
+  return [dudeMode, toggleDude];
+};
