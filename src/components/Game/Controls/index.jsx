b4a20d09dda2fd51dfc65277b7c59039ac0e3e9e
gebhartn <nicholas.gebhart@gmail.com>
2020-03-05 10:11:04 -0800

    feat: renders rooms

--- src/components/Game/Controls/index.jsx
+++ src/components/Game/Controls/index.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { axiosWithAuth } from '../../../services/authServices';
 
-const Controls = ({ gameState, setGameState }) => {
+const Controls = ({ setPlayerState, playerState }) => {
   const handleClick = async (e, body) => {
     e.preventDefault();
     const send = { direction: body };
-    // Parse direction player is moving to send to move endpoint
-    // Additionally, set player current position to match new location
-    //
-    // Any way to disable buttons based on description parse?
-
     const { data } = await axiosWithAuth().post('api/adv/move/', send);
 
-    setGameState(data);
+    setPlayerState(data);
 
     return data;
   };
@@ -23,17 +18,22 @@ const Controls = ({ gameState, setGameState }) => {
         // W to go North
         case 87: {
           const send = { direction: 'n' };
-          const { data } = await axiosWithAuth().post('api/adv/move/', send);
 
-          setGameState(data);
+          const { data } = await axiosWithAuth().post('api/adv/move/', send);
+          const { x_cor, y_cor } = data;
+          console.log({ x_cor, y_cor });
+
+          setPlayerState({ x: x_cor, y: y_cor });
           return data;
         }
         // S to go South
         case 83: {
           const send = { direction: 's' };
           const { data } = await axiosWithAuth().post('api/adv/move/', send);
+          const { x_cor, y_cor } = data;
 
-          setGameState(data);
+          setPlayerState({ x: x_cor, y: y_cor });
+          console.log(playerState);
           return data;
         }
         // A to go West
@@ -41,7 +41,7 @@ const Controls = ({ gameState, setGameState }) => {
           const send = { direction: 'w' };
           const { data } = await axiosWithAuth().post('api/adv/move/', send);
 
-          setGameState(data);
+          setPlayerState(data);
           return data;
         }
         // D to go East
@@ -49,7 +49,7 @@ const Controls = ({ gameState, setGameState }) => {
           const send = { direction: 'e' };
           const { data } = await axiosWithAuth().post('api/adv/move/', send);
 
-          setGameState(data);
+          setPlayerState(data);
           return data;
         }
         default:
@@ -57,7 +57,7 @@ const Controls = ({ gameState, setGameState }) => {
       }
       return null;
     },
-    [setGameState]
+    [setPlayerState, playerState]
   );
 
   React.useEffect(() => {
