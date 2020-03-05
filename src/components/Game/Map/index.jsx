67f927c1783357e6ebdcc1d2deeee9ffc8b45d96
gebhartn <nicholas.gebhart@gmail.com>
2020-03-05 10:11:14 -0800

    chore: updates map state

--- src/components/Game/Map/index.jsx
+++ src/components/Game/Map/index.jsx
@@ -1,48 +1,89 @@
 import React from 'react';
 import { VictoryGroup, VictoryLine, VictoryScatter } from 'victory';
 
-const Map = ({ mapState }) => {
-  const [player, setPlayer] = React.useState({ x: 0, y: 0 });
-  const lineRaw = [
-    { x: 1, y: 2 },
-    { x: 2, y: 1 }
-  ];
-
-  // Send post and setPlayer to response.coordinates
-  const handlePlayer = points => {
-    const { x, y } = points;
-    setPlayer({ x, y });
+const Map = ({ mapState, playerState }) => {
+  const northLines = d => {
+    if (d.north_id) {
+      return [
+        { x: d.x_cor, y: d.y_cor },
+        { x: d.x_cor, y: d.y_cor + 1 }
+      ];
+    }
   };
 
+  const westLines = d => {
+    if (d.west_id) {
+      return [
+        { x: d.x_cor, y: d.y_cor },
+        { x: d.x_cor - 1, y: d.y_cor }
+      ];
+    }
+  };
+
+  const eastLines = d => {
+    if (d.east_id) {
+      return [
+        { x: d.x_cor, y: d.y_cor },
+        { x: d.x_cor + 1, y: d.y_cor }
+      ];
+    }
+  };
+
+  const southLines = d => {
+    if (d.south_id) {
+      return [
+        { x: d.x_cor, y: d.y_cor },
+        { x: d.x_cor, y: d.y_cor - 1 }
+      ];
+    }
+  };
+
+  const north = mapState.map(dungeon => northLines(dungeon)).filter(x => x);
+  const south = mapState.map(dungeon => southLines(dungeon)).filter(x => x);
+  const west = mapState.map(dungeon => westLines(dungeon)).filter(x => x);
+  const east = mapState.map(dungeon => eastLines(dungeon)).filter(x => x);
+
   const scatterData = mapState.map(point => {
-    if (point.x_cor === player.x && point.y_cor === player.y) {
+    if (point.x_cor === playerState.x && point.y_cor === playerState.y) {
       return {
         x: point.x_cor,
         y: point.y_cor,
         symbol: 'star',
         opacity: 1,
-        label: `Playa`,
-        fill: 'blue'
+        // label: `Playa`,
+        fill: 'red',
+        size: 4
       };
     }
     return { x: point.x_cor, y: point.y_cor, symbol: 'circle' };
   });
 
   return (
-    <div style={{ width: '500px', height: '500px' }}>
-      <VictoryGroup>
-        <VictoryLine data={lineRaw} />
+    <div
+      style={{
+        width: '960px',
+        height: '960px'
+      }}
+    >
+      <VictoryGroup color="#888888">
+        {north.map(pair => {
+          return <VictoryLine data={pair} />;
+        })}
+        {south.map(pair => {
+          return <VictoryLine data={pair} />;
+        })}
+        {west.map(pair => {
+          return <VictoryLine data={pair} />;
+        })}
+        {east.map(pair => {
+          return <VictoryLine data={pair} />;
+        })}
+        {/* <VictoryLine data={lineRaw} /> */}
         <VictoryScatter
           data={scatterData}
           style={{ data: { fill: ({ datum }) => datum.fill } }}
         />
       </VictoryGroup>
-
-      <button onClick={() => handlePlayer({ x: 0, y: 1 })}>click me</button>
-      <button onClick={() => handlePlayer({ x: 0, y: 1 })}>click me</button>
-      <button onClick={() => handlePlayer({ x: 0, y: 1 })}>click me</button>
-      <button onClick={() => handlePlayer({ x: 0, y: 1 })}>click me</button>
-      <button onClick={() => handlePlayer({ x: 0, y: 1 })}>click me</button>
     </div>
   );
 };
