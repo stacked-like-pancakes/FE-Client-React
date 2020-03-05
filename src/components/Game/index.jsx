419dddd656d2c4e51f826948d40b2116cca2c166
gebhartn <nicholas.gebhart@gmail.com>
2020-03-05 10:11:25 -0800

    chore: updates game state

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 import { axiosWithAuth } from '../../services/authServices';
 
 import Map from './Map';
 import Controls from './Controls';
 
+const Container = styled.div`
+  margin: 10vh auto 0;
+  width: 60%;
+  height: 80vh;
+`;
+
 const Game = () => {
-  const [gameState, setGameState] = React.useState({});
   const [mapState, setMapState] = React.useState([]);
+  const [playerState, setPlayerState] = React.useState({ x: null, y: null });
 
   React.useEffect(() => {
     (async () => {
-      const { data } = await axiosWithAuth().get(`api/adv/rooms`);
+      const result = await axiosWithAuth().get('api/adv/rooms/');
+      setMapState(result.data.Dungeon);
+    })();
+  }, []);
 
-      setMapState(data.dungeon);
+  React.useEffect(() => {
+    (async () => {
+      // const { data } = await axiosWithAuth().get(`api/adv/rooms`);
+
+      const result = await axiosWithAuth().get('api/adv/init/');
+      const { x_cor, y_cor } = result.data;
+      console.log('here', { x_cor, y_cor });
+      setPlayerState({ x: x_cor, y: y_cor });
     })();
   }, []);
 
   return (
-    <>
-      <Map mapState={mapState} setMapState={setMapState} />
-      <Controls gameState={gameState} setGameState={setGameState} />
-    </>
+    <Container>
+      <Map
+        playerState={playerState}
+        setPlayerState={setPlayerState}
+        mapState={mapState}
+        setMapState={setMapState}
+      />
+      <Controls playerState={playerState} setPlayerState={setPlayerState} />
+    </Container>
   );
 };
 
